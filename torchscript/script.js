f89b9f8b12a7041173678af63462ea68cdc63880
David Riazati <davidriazati@fb.com>
2019-09-18 16:24:10 -0700

    update

--- torchscript/script.js
+++ torchscript/script.js
@@ -9,26 +9,53 @@ c3.chart.internal.fn.hideTooltip = function () { };
 
 function make_chart(name) {
 	d3.csv(name + ".csv").then(csv => {
-		columns = [];
 		ignore_columns = {
-			'git_hash': true
+			'git_hash': true,
+			'benchmark_time': true,
+			'commit_pr': true,
+			'run_number': true,
+			'commit_time': true,
 		};
-		name_to_idx = {};
+
+		let column_names = [];
+		let columns = [];
+		let name_to_idx = {};
+
 		for (let key in csv[0]) {
-			name_to_idx[key] = columns.length;
+			name_to_idx[key] = column_names.length;
 			if (!ignore_columns[key]) {
-				columns.push([key])
+				column_names.push(key);
+				columns.push([key]);
 			}
 		}
 
-		for (let i = 0; i < csv.length; i++) {
-			for (let key in csv[i]) {
-				const index = name_to_idx[key];
-				if (!ignore_columns[key]) {
-					columns[index].push(csv[i][key])
-				}
+		// Get the x-column data
+		let x_data = d3.nest()
+			.key(d => d['git_hash'])
+			.rollup(d => {
+				return d[0].commit_time;
+			})
+			.entries(csv)
+		x_data = x_data.map(d => d.value);
+		let x_column = ["commit_time"].concat(x_data)
+
+		// Get the mean of each column
+		for (let i = 0; i < column_names.length; i++) {
+			let name = column_names[i];
+
+			let data = d3.nest()
+				.key(d => d['git_hash'])
+				.rollup(d => {
+					return d3.mean(d, x => x[name]);
+				})
+				.entries(csv)
+			for (let j = 0; j < data.length; j++) {
+				columns[i].push(data[j].value);
 			}
 		}
+
+		columns.push(x_column);
+
 		const div = document.createElement('div');
 		div.id = name + '_chart';
 		document.getElementById('charts').appendChild(div);
@@ -36,7 +63,7 @@ function make_chart(name) {
 		var chart = c3.generate({
 			bindto: '#' + div.id,
 			data: {
-				x: 'time',
+				x: 'commit_time',
 				xFormat: '%Y-%m-%d %H:%M:%S',
 				columns: columns
 			},
@@ -60,13 +87,25 @@ function make_chart(name) {
 					let item_obj = csv[d[0].index];
 					let items = [];
 					for (let key in item_obj) {
+						if (key == 'git_hash') {
+							items.push({
+								"key": 'Git Hash',
+								"value": item_obj[key].substr(0, 10)
+							});
+							continue;
+						}
+						if (ignore_columns[key]) {
+							continue;
+						}
+						let value = +item_obj[key];
 						items.push({
 							"key": key,
-							"value": item_obj[key]
+							"value": +value.toFixed(2)
 						})
 					}
 					const tooltip = document.createElement('div');
-					const table = d3.select(tooltip).append('table');
+					const table = d3.select(tooltip).append('table')
+						.classed('tooltip-table', true);
 
 					const rows = table.append('tbody').selectAll('tr')
 						.data(items)
