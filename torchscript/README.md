3d1279fb48dc733763860afb6a43a193e44b3cb9
Your Name <davidriazati@fb.com>
2019-12-04 12:41:48 -0800

    stuff

--- torchscript/README.md
+++ torchscript/README.md
@@ -2,20 +2,60 @@
 
 ## Resnet50
 
-
 <div id='basic'></div>
 
 
 # Reproducing Results
 
-1. Disable `chef`
+* Use `Benchmark.cleanup()` liberally in your benchmark tests to do garbage collection and clear caches
+* Disable chef with `$ stop_chef_temporarily`
 
 
 # Adding a Benchmark
 
+Benchmarks are written in a test file which is then run many times as a subprocess
+of an orchestration script [`runner/main.py`](). **By default, the runner script
+will uninstall PyTorch and torchvision (use `--skip-checkout` to disable)**. The
+runner script relies on having a conda environment set up. You may want to add
+one specifically for benchmarking
+
+```bash
+$ conda create --name benchmarking python=3.7
+$ conda activate benchmarking
+```
+
+The runner script can be used as follows
+
+```
+$ python runner/main.py -h
+usage: main.py [-h] [--skip-checkout] [--skip-conda-check] --out OUT
+               [--hash HASH]
+
+Run TorchScript benchmarks
+
+optional arguments:
+  -h, --help          show this help message and exit
+  --skip-checkout     Don't remove existing PyTorch/Torchvision
+  --skip-conda-check  Don't print the current conda environment
+  --out OUT           Destination git repo to write JSONs to
+  --hash HASH         PyTorch hash to use
+```
+
+If a PyTorch git hash is provided via `--hash`, the runner script will remove any
+existing installs of PyTorch and torchvision, then check out
+that commit and build it. This allows for easy backtesting of results,
+for an example see [`examples/backtest.py`]().
+
+If you are trying to test against some local changes to PyTorch, use the `--skip-checkout`
+option to prevent the runner from uninstalling and building anything and skip straight
+to running the benchmarks.
+
+If `--out` is not provided, results will be printed to `stdout` instead of written to a
+JSON file in the directory specified to `--out`. For an example see [`examples/backtest.py`]().
+
 ## Generate Results
 
-1. Write your benchmark test in [`test.py`]().
+1. Write your benchmark test in [`benchmarks/test.py`]().
 2. Use the [`runner.py`]() to run the test and generate results. These will be saved to a `.json` file and `git push`ed to the `driazati.github.io/torchscript/data` folder.
 
 ## Display Results
@@ -45,3 +85,12 @@
 4. View the changes by running `python -m http.server` and going to `localhost:8000` in your browser.
 
 5. `git push` the changes
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.8/c3.css">
+<link rel="stylesheet" href="styles.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.12.0/d3.js" charset="utf-8"></script>
+<script src="https://d3js.org/d3-array.v2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.7.8/c3.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@handsontable/jstat/dist/jstat.min.js"></script>
+<script src='script.js'></script>
--- torchscript/script.js
+++ torchscript/script.js
@@ -1,19 +1,20 @@
+console.log("Generating charts...")
 make_chart({
-	data_file: 'data/resnet50.json',
-	div_id: 'basic',
-	xlabel: 'Commit Time',
-	ylabel: 'Milliseconds',
-	title: 'Basic',
-	rollups: [
-		{
-			"name": "mean",
-			"fn": d3.mean
-		},
-		{
-			"name": "variance",
-			"fn": d3.variance
-		}
-	]
+    data_file: 'data/resnet50.json',
+    div_id: 'basic',
+    xlabel: 'Commit Time',
+    ylabel: 'Milliseconds',
+    title: 'Median Execution Time',
+    rollups: [
+        {
+            "name": "median",
+            "fn": d3.median
+        },
+        {
+            "name": "variance",
+            "fn": d3.variance
+        }
+    ]
 });
 
 // Make tooltips stay up so they can be clicked
@@ -22,105 +23,105 @@ c3.chart.internal.fn.hideTooltip = function () { };
 
 
 function make_chart(options) {
-	d3.json(options.data_file).then(json => {
-		let rollup = options.rollups[0];
+    d3.json(options.data_file).then(json => {
+        let rollup = options.rollups[0];
 
-		// commit hash -> { ... }
+        // commit hash -> { ... }
 
-		// ['commit_time', a, b, c]
-		// ['metric 1', a_m1, b_m1, c_m1]
-		json.forEach(d => {
-			d.commit.time = new Date(d.commit.time)
-		})
+        // ['commit_time', a, b, c]
+        // ['metric 1', a_m1, b_m1, c_m1]
+        json.forEach(d => {
+            d.commit.time = new Date(d.commit.time)
+        })
 
-		// Get the data for each time step for the x-axis
-		let x_data = json.map(d => d.commit.time);
-		let x_column = ["commit_time"].concat(x_data);
+        // Get the data for each time step for the x-axis
+        let x_data = json.map(d => d.commit.time);
+        let x_column = ["commit_time"].concat(x_data);
 
-		let all_series = [x_column];
+        let all_series = [x_column];
 
-		let sample_entry = json[0].runs[0];
-		let fields = Object.keys(sample_entry).filter(d => d !== 'benchmark_run_at')
-		for (let i = 0; i < fields.length; i++) {
-			let series = [fields[i]];
+        let sample_entry = json[0].runs[0];
+        let fields = Object.keys(sample_entry).filter(d => d !== 'benchmark_run_at')
+        for (let i = 0; i < fields.length; i++) {
+            let series = [fields[i]];
 
-			json.forEach(d => {
-				let value = rollup.fn(d.runs[0][fields[i]]);
-				series.push(value);
-			})
+            json.forEach(d => {
+                let value = rollup.fn(d.runs[0][fields[i]]);
+                series.push(value);
+            })
 
-			all_series.push(series);
-		}
+            all_series.push(series);
+        }
 
-		var chart = c3.generate({
-			bindto: '#' + options.div_id,
-			data: {
-				x: 'commit_time',
-				xFormat: '%Y-%m-%dT%H:%M:%S%z',
-				// xFormat: '%Y-%m-%d %H:%M:%S',
-				columns: all_series
-			},
-			axis: {
-				x: {
-					type: 'timeseries',
-					tick: {
-						format: '%m-%d'
-					},
-					label: options.xlabel
-				},
-				y: {
-					label: options.ylabel
-				}
-			},
-			zoom: {
-				enabled: true,
-				// type: 'drag',
-				rescale: true
-			},
-			title: {
-				text: options.title
-			},
-			tooltip: {
-				contents: (d, defaultTitleFormat, defaultValueFormat, color) => {
-					let datum = json[d[0].index];
+        var chart = c3.generate({
+            bindto: '#' + options.div_id,
+            data: {
+                x: 'commit_time',
+                xFormat: '%Y-%m-%dT%H:%M:%S%z',
+                // xFormat: '%Y-%m-%d %H:%M:%S',
+                columns: all_series
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%m-%d'
+                    },
+                    label: options.xlabel
+                },
+                y: {
+                    label: options.ylabel
+                }
+            },
+            zoom: {
+                enabled: true,
+                // type: 'drag',
+                rescale: true
+            },
+            title: {
+                text: options.title
+            },
+            tooltip: {
+                contents: (d, defaultTitleFormat, defaultValueFormat, color) => {
+                    let datum = json[d[0].index];
 
-					let items = [
-						{
-							key: "Git Hash",
-							value: datum.commit.hash.substr(0, 10)
-						}
-					];
+                    let items = [
+                        {
+                            key: "Git Hash",
+                            value: datum.commit.hash.substr(0, 10)
+                        }
+                    ];
 
-					for (let i = 0; i < fields.length; i++) {
-						let value = rollup.fn(datum.runs[0][fields[i]]);
+                    for (let i = 0; i < fields.length; i++) {
+                        let value = rollup.fn(datum.runs[0][fields[i]]);
 
-						items.push({
-							"key": fields[i],
-							"value": +value.toFixed(2)
-						});
-					}
+                        items.push({
+                            "key": fields[i],
+                            "value": +value.toFixed(2)
+                        });
+                    }
 
-					const tooltip = document.createElement('div');
-					const table = d3.select(tooltip).append('table')
-						.classed('tooltip-table', true);
+                    const tooltip = document.createElement('div');
+                    const table = d3.select(tooltip).append('table')
+                        .classed('tooltip-table', true);
 
-					const rows = table.append('tbody').selectAll('tr')
-						.data(items)
-						.enter()
-						.append('tr');
+                    const rows = table.append('tbody').selectAll('tr')
+                        .data(items)
+                        .enter()
+                        .append('tr');
 
-					rows.selectAll('td')
-					.data((row) => {
-						return [row.key, row.value]
-					})
-					.enter().append('td').text(d => d)
+                    rows.selectAll('td')
+                    .data((row) => {
+                        return [row.key, row.value]
+                    })
+                    .enter().append('td').text(d => d)
 
-					return tooltip.innerHTML;
-				}
-			}
-		});
+                    return tooltip.innerHTML;
+                }
+            }
+        });
 
-	});
+    });
 }
 
 
