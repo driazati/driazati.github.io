ecb5872405a5c5a4f06d5559330dbcc746b7a199
Your Name <you@example.com>
2020-05-10 17:10:17 -0700

    Working map

---
+++ test/test.py
@@ -0,0 +1,14 @@
+from p5 import *
+
+def setup():
+    size(640, 360)
+    no_stroke()
+    background(204)
+
+def draw():
+    fill(255, 15)
+    circle((100, 100), 40)
+
+    # add more stuff here
+
+run()
